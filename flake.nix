9811d021b13e5bc35d4711f402cb920bab0244c3
illusaen <jaewchen@gmail.com>
2026-04-25 00:23:51 -0500

    Handle missing dirtyRev in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         (system: fn system (overlayPkgs nixpkgs.legacyPackages.${system}));
   in {
     overlays.default = import ./overlay.nix {
-      rev = self.rev or self.dirtyRev;
+      rev = self.rev or (if self ? dirtyRev then self.dirtyRev else "NOVERSION");
     };
 
     packages = forEachSystem (system: pkgs: rec {
